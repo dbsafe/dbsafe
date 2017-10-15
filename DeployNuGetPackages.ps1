55e110cdfa1c905ba36c16f999267167d8bd808f
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-10-15 14:19:20 -0500

    CI: DeployNuGetPackages

---
+++ DeployNuGetPackages.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = "Stop"
+
+# ls
+
+Write-Host Deployment Script
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.nupkg -Exclude *.symbols.nupkg -Recurse -File -Name | ForEach-Object {
+    if ($_ -match "bin\\Release") {
+        Write-Host Publishing $_
+        nuget.exe push $_ -Source https://www.nuget.org/api/v2/package -ApiKey $env:api_key
+    }
+}
