8cd0c3c0189f8464d15ab2a41d09306be1ef4e17
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-10-15 07:25:42 -0500

    Added UpdateBuildVersion.ps1

---
+++ UpdateBuildVersion.ps1
@@ -0,0 +1,40 @@
+$ErrorActionPreference = "Stop"
+
+$assemblyVersion = "1.2.3.6"
+
+Write-Host Setting AssemblyVersion and FileVersion to: $assemblyVersion
+
+# ls
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.csproj -Recurse -File -Name | ForEach-Object {
+    $projectName = [System.IO.Path]::GetFullPath($_)    
+    Write-Host Searching Project: $projectName
+    $updated = 0
+
+
+    $xml = [xml](Get-Content $projectName)
+    $nodes = $xml.Project.ChildNodes
+    foreach ($node in $nodes) {
+        #Write-Host $node.ToString()
+        if ($node.Name -eq "PropertyGroup") {
+            
+            $current = "[" + $node.AssemblyVersion + "]"
+            if ($current -ne "[]") {
+                $node.AssemblyVersion = $assemblyVersion
+                $updated = 1;
+            }
+
+            $current = "[" + $node.FileVersion + "]"
+            if ($current -ne "[]") {
+                $node.FileVersion = $assemblyVersion
+                $updated = 1;
+            }
+        }
+    }
+
+    if ($updated -eq 1) {
+        Write-Host Updated Project: $projectName
+        $xml.Save($projectName)
+    }
+}
+
