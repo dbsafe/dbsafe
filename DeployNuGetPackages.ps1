42ec93f6520ad99f8f0c46ce6f221e9ba3cba63d
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-10-22 16:30:51 -0500

    CI: Disabled deployment.

--- DeployNuGetPackages.ps1
+++ DeployNuGetPackages.ps1
@@ -6,7 +6,7 @@ Write-Host Deployment Script
 
 Get-ChildItem -Path $PSScriptRoot -Filter *.nupkg -Exclude *.symbols.nupkg -Recurse -File -Name | ForEach-Object {
     if ($_ -match "bin\\Release") {
-        Write-Host Publishing $_
-        nuget.exe push $_ -Source https://www.nuget.org/api/v2/package -ApiKey $env:api_key
+        # Write-Host Publishing $_
+        # nuget.exe push $_ -Source https://www.nuget.org/api/v2/package -ApiKey $env:api_key
     }
 }
