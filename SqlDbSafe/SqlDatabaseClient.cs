67aea8a9f203f0e877f6431992e252fad8a0af62
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-11-11 15:46:14 -0600

    Added PgDbSafe project to support postgres database.

--- SqlDbSafe/SqlDatabaseClient.cs
+++ SqlDbSafe/SqlDatabaseClient.cs
@@ -13,44 +13,46 @@ namespace SqlDbSafe
     {
         public override void WriteTable(DatasetElement dataset)
         {
-            if (dataset.Data != null)
+            if (dataset.Data == null)
             {
-                bool isDatabseEmpty = !dataset.Data.Elements().Any();
-                if (isDatabseEmpty)
-                {
-                    return;
-                }
-
-                StringBuilder sb = new StringBuilder();
-                if (dataset.SetIdentityInsert)
-                {
-                    sb.AppendFormat("SET IDENTITY_INSERT {0} ON;", dataset.Table);
-                    sb.AppendLine();
-                }
-
-                foreach (var element in dataset.Data.Elements())
-                {
-                    var queryFieldNamesArray = element.Attributes().Select(a => a.Name.ToString()).ToArray();
-                    var queryFieldNames = string.Join(", ", queryFieldNamesArray);
-
-                    var queryFieldValuesArray = element.Attributes().Select(a => a.Value).ToArray();
-                    queryFieldValuesArray = queryFieldValuesArray
-                        .Select(a => string.Format("'{0}'", a))
-                        .ToArray();
-                    var queryFieldValues = string.Join(", ", queryFieldValuesArray);
-
-                    var insert = string.Format("INSERT INTO {0} ({1}) VALUES ({2});", dataset.Table, queryFieldNames, queryFieldValues);
-                    sb.AppendLine(insert);
-                }
-
-                if (dataset.SetIdentityInsert)
-                {
-                    sb.AppendFormat("SET IDENTITY_INSERT {0} OFF;", dataset.Table);
-                    sb.AppendLine();
-                }
-
-                ExecuteCommand(sb.ToString());
+                return;
             }
+
+            bool isDatabseEmpty = !dataset.Data.Elements().Any();
+            if (isDatabseEmpty)
+            {
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            if (dataset.SetIdentityInsert)
+            {
+                sb.AppendFormat("SET IDENTITY_INSERT {0} ON;", dataset.Table);
+                sb.AppendLine();
+            }
+
+            foreach (var element in dataset.Data.Elements())
+            {
+                var queryFieldNamesArray = element.Attributes().Select(a => a.Name.ToString()).ToArray();
+                var queryFieldNames = string.Join(", ", queryFieldNamesArray);
+
+                var queryFieldValuesArray = element.Attributes().Select(a => a.Value).ToArray();
+                queryFieldValuesArray = queryFieldValuesArray
+                    .Select(a => string.Format("'{0}'", a))
+                    .ToArray();
+                var queryFieldValues = string.Join(", ", queryFieldValuesArray);
+
+                var insert = string.Format("INSERT INTO {0} ({1}) VALUES ({2});", dataset.Table, queryFieldNames, queryFieldValues);
+                sb.AppendLine(insert);
+            }
+
+            if (dataset.SetIdentityInsert)
+            {
+                sb.AppendFormat("SET IDENTITY_INSERT {0} OFF;", dataset.Table);
+                sb.AppendLine();
+            }
+
+            ExecuteCommand(sb.ToString());
         }
 
         protected override SqlConnection CreateDbConnection(string connectionString) => new SqlConnection(connectionString);
