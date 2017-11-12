b4ed606dc2fffe40e5ba5f26d86cb2867726f601
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-11-12 10:21:46 -0600

    Added PgDbSafeManager.

---
+++ PgDbSafe/PgDbSafeManager.cs
@@ -0,0 +1,30 @@
+﻿using DbSafe;
+using Npgsql;
+
+namespace PgDbSafe
+{
+    public class PgDbSafeManager : AdoDbSafeManager<PgDatabaseClient, NpgsqlConnection, NpgsqlCommand>
+    {
+        public static PgDbSafeManager Initialize(DbSafeManagerConfig config, params string[] filenames)
+        {
+            var result = new PgDbSafeManager(config);
+            foreach (var filename in filenames)
+            {
+                result.Load(filename);
+            }
+
+            result.BeginTest();
+            return result;
+        }
+
+        public static PgDbSafeManager Initialize(params string[] filenames)
+        {
+            return Initialize(DbSafeManagerConfig.GlobalConfig, filenames);
+        }
+
+        private PgDbSafeManager(DbSafeManagerConfig config)
+            : base(config, new PgDatabaseClient())
+        {
+        }
+    }
+}
